baa77efe3bcc45d5262a126d9b0c0a48a25f7309
葛梦甜 <1711285659@qq.com>
2019-04-11 19:28:15 +0800

    swiper npm

---
+++ src/components/Banner.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="swiper-container billboards">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide"
+      v-for="(item,index) in banners"
+      :key="index">
+        <img :src="item.imgUrl" alt>
+      </div>
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+</template>
+
+<script>
+const Swiper = window.Swiper;
+export default {
+  props:{ 
+    banners: {
+      type: Array
+    }
+  },
+  data() {
+    return {
+      swiper: null
+    }
+  },
+
+  updated() {
+    if(!this.swiper){
+      this.swiper = new Swiper('.swiper-container',{
+        loop: true,
+          pagination: {
+          el: '.swiper-pagination',
+        },
+      })
+    } 
+  }
+}
+</script>
+
+<style lang="less">
+.swiper-container{
+    width: 100%;
+    height: 210px;
+    
+    img{
+      width: 100%;
+    }
+
+    .swiper-pagination-bullet{
+      background: #fff;
+    }
+    .swiper-pagination-bullet-active{
+      background: #fff;
+    }
+}
+</style>
--- src/views/Film.vue
+++ src/views/Film.vue
@@ -1,6 +1,44 @@
 <template>
   <div>
-    电影首页
+    <Banner :banners='bannerList'></Banner>
   </div>
    
 </template>
+
+<script>
+import Banner from '../components/Banner';
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      bannerList: []
+    }
+  },
+  components: {
+    Banner
+  },
+
+  methods: {
+    getBannerList() {
+      axios.get('https://m.maizuo.com/gateway?type=2&cityId=440300&k=4183831',{
+        headers: {
+          'X-Client-Info': '{"a":"3000","ch":"1002","v":"1.0.0","e":"15547213092100239007919"}',
+          'X-Host': 'mall.cfg.common-banner'
+        }
+      }).then(res => {
+        let data = res.data;
+        if(data.status === 0 ){
+          this.bannerList = data.data;
+        }else{
+          alert(data.msg);
+        }
+      })
+    }
+  },
+
+  created() {
+    this.getBannerList();
+  },
+}
+</script>
