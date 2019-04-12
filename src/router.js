f7b33eccc09a160eef5b25c3b1052a6f764d7548
葛梦甜 <1711285659@qq.com>
2019-04-12 16:57:00 +0800

    路由嵌套

--- src/router.js
+++ src/router.js
@@ -6,16 +6,25 @@ Vue.use(VueRouter);
 export default new VueRouter({
     routes: [
         { 
-            path: '/films',  
-            component: () => import('./views/Film.vue'),
-            children: [
-                { path: 'nowPlaying', component: () => import('./components/FilmNowPlaying.vue')},
-                { path: 'comingSoon', component: () => import('./components/FilmComingSoon.vue')},
-                { path: '', redirect: '/films/nowPlaying'}
-            ]
+					path: '/', 
+					component: () => import('./views/Home.vue'),
+					children: [
+							{ 
+								path: 'films',  
+								component: () => import('./views/Film.vue'),
+								children: [
+									{ path: 'nowPlaying', component: () => import('./components/FilmNowPlaying.vue')},
+									{ path: 'comingSoon', component: () => import('./components/FilmComingSoon.vue')},
+									{ path: '', redirect: '/films/nowPlaying'}
+									]
+							},
+							{ path: 'cinemas', component: () => import('./views/Cinema.vue') },
+							{ path: 'center', component: () => import('./views/Center.vue') },
+							{ path: '', redirect: '/films/nowPlaying' },
+					]
         },
-        { path: '/cinemas', component: () => import('./views/Cinema.vue') },
-        { path: '/center', component: () => import('./views/Center.vue') },
-        { path: '/film/:id', name: 'detail', component: () => import('./views/Detail.vue') },
+        
+				{ path: '/film/:id', name: 'detail', component: () => import('./views/Detail.vue') },
+				{ path: '*', redirect: '/films/nowPlaying' },
     ]
 })
